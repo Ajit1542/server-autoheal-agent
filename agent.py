7cf99c999b8c8b70d78d7d3210a09fe6c01afbc6
Ajit1542 <ajitghawghawe@gmail.com>
2026-02-09 15:59:56 +0530

    added metrics for prometheus

--- agent.py
+++ agent.py
@@ -5,9 +5,18 @@ from modules import (
     host_monitor,
     fstab_check
 )
-
 import datetime
-
+METRICS_FILE = "metrics.prom"
+metrics = {
+    "autoheal_service_failed": 0,
+    "autoheal_disk_var_failed": 0,
+    "autoheal_stale_failed": 0,
+    "autoheal_fstab_failed": 0
+}
+def write_metrics():
+    with open(METRICS_FILE, "w") as f:
+        for k, v in metrics.items():
+            f.write(f"{k} {v}\n")
 def log(message):
     with open("logs/agent.log", "a") as f:
         f.write(f"{datetime.datetime.now()} - {message}\n")
@@ -18,9 +27,18 @@ if __name__ == "__main__":
     service_monitor.run()
     disk_var.run()
     stale_mount.run()
-    host_monitor.run()
+    # host_monitor.run()
     fstab_check.run()
     
+    if not disk_var.run():
+        metrics["autoheal_disk_var_failed"]=1
+    if not service_monitor.run():
+        metrics["autoheal_service_failed"]=1
+    if not fstab_check.run():
+        metrics["autoheal_fstab_failed"]=1
+    if not stale_mount.run():
+        metrics["autoheal_stale_failed"]=1
+        
     log("===== Agent Finished =====\n")
 
 
